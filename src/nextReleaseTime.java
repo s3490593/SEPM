83be87c3d3aca9e9fc575acd1a41791577e5d2fb
HungyeeLinS3790956 <S3790956@student.rmit.edu.au>
2022-05-13 09:57:41 +0800

    Update nextReleaseTimemethod and Add PBI2's unit test

--- src/nextReleaseTime.java
+++ src/nextReleaseTime.java
@@ -4,20 +4,23 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class nextReleaseTime {
-
 	public static void main(String[] args) {
 		//try to make sure that we can get correct next release time
 		nextReleaseTime nrtNotify = new nextReleaseTime();
 		String todayAns = "GROUP";
 		String[] guessAns = { "SCENE", "MAPLE" };
-		String msg = nrtNotify.notify(nrtNotify.getNextReleaseTime(), true, false, todayAns, guessAns);
+		boolean isAnsCorrect = true;
+		boolean isFinish = true;
+
+		String msg = nrtNotify.notify(nrtNotify.getNextReleaseTime(), isAnsCorrect, isFinish, todayAns, guessAns);
+		nrtNotify.displayNextReleaseTime(isFinish);
 	}
 
 	public String getNextReleaseTime() {
 		// using 86400(total second in a day) - current Time(second) to get next release
 		// time
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
-		String datetime[] = dtf.format(LocalDateTime.now()).toString().split(":");
+		String datetime[] = dtf.format(LocalDateTime.now()).split(":");
 		int totalSecond = 86400;
 		int restSecond = totalSecond - (Integer.parseInt(datetime[0]) * 3600 + Integer.parseInt(datetime[1]) * 60
 				+ Integer.parseInt(datetime[2]));
@@ -31,16 +34,15 @@ public class nextReleaseTime {
 	public String notify(String releaseTime, boolean isAnsCorrect, boolean isFinish, String ans, String[] guessAns) {
 		// return different response based on different condition
 		String responseMsg = null;
-		boolean isActive = false;
+//		boolean isActive = false;
 
 		if (isFinish) {
 			responseMsg = "Too Bad! Todays word is [ " + ans + " ] !";
 
 			if (isAnsCorrect) {
 				responseMsg = "Congratulations! You guessed [ " + ans + " ] correctly!";
-
 			}
-			isActive = true;
+//			isActive = true;
 		} else {
 			responseMsg = "The user's current guess result is : ";
 			for (String item : guessAns) {
@@ -48,18 +50,16 @@ public class nextReleaseTime {
 			}
 		}
 		System.out.println(responseMsg);
-		this.displayNextReleaseTime(isActive);
+//		this.displayNextReleaseTime(isActive);
 
 		return responseMsg;
 	}
 
-	public void displayNextReleaseTime(boolean isActive){
-		if(isActive){
+	public void displayNextReleaseTime(boolean isFinish){
+		if(isFinish){
 			TimerTask timerTask = new TimerTask() {
-
 				@Override
 				public void run() {
-
 					System.out.println("The Next Release Time is : "+new nextReleaseTime().getNextReleaseTime());
 				}
 			};
