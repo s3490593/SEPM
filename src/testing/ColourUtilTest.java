34aa85e80fdf7dfc4e3c30bb0a619094ef15ce41
[Yuzhao Qian s3913010] <s3913010@student.rmit.edu.au>
2022-05-22 17:24:16 +0800

    Update unit test for ColourUtilTest.java and WordMatchUtilTest.java

--- src/testing/ColourUtilTest.java
+++ src/testing/ColourUtilTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ColourUtilTest {
 
     @Test
-    void coloring() {
+    void coloring_partial_coloring() {
         String[] str= new String[5];
         str[0] = "h";
         str[1] = "e";
@@ -29,4 +29,30 @@ class ColourUtilTest {
         assertEquals(expectResult, result);
 
     }
+    @Test
+    void coloring_all_coloring() {
+        String[] str= new String[5];
+        str[0] = "h";
+        str[1] = "e";
+        str[2] = "l";
+        str[3] = "l";
+        str[4] = "o";
+
+        String[] expectStr = str.clone();
+
+        expectStr[0]= "\u001B[32m"+expectStr[0]+"\u001B[0m";
+        expectStr[1]= "\u001B[32m"+expectStr[1]+"\u001B[0m";
+        expectStr[2]= "\u001B[32m"+expectStr[2]+"\u001B[0m";
+        expectStr[3]= "\u001B[32m"+expectStr[3]+"\u001B[0m";
+        expectStr[4]= "\u001B[32m"+expectStr[4]+"\u001B[0m";
+
+
+
+        String expectResult = String.join("",expectStr);
+        String result=String.join("", ColourUtil.coloring(str,0,4,"GREEN"));
+
+        assertEquals(expectResult, result);
+
+    }
+
 }
--- src/testing/WordMatchUtilTest.java
+++ src/testing/WordMatchUtilTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class WordMatchUtilTest {
 
     @Test
-    void doMatch() {
+    void doMatch1() {
         String[] str= new String[5];
         str[0] = "s";
         str[1] = "u";
@@ -24,4 +24,21 @@ class WordMatchUtilTest {
         String result=String.join("", WordMatchUtil.doMatch(str,"messy"));
         assertEquals(expectResult, result);
     }
+    @Test
+    void doMatch2() {
+        String[] str= new String[5];
+        str[0] = "n";
+        str[1] = "o";
+        str[2] = "i";
+        str[3] = "s";
+        str[4] = "y";
+
+        String[] expectStr = str.clone();
+        expectStr[3]= "\u001B[32m"+expectStr[3]+"\u001B[0m";
+        expectStr[4]= "\u001B[32m"+expectStr[4]+"\u001B[0m";
+        String expectResult = String.join("",expectStr);
+        String result=String.join("", WordMatchUtil.doMatch(str,"messy"));
+        assertEquals(expectResult, result);
+    }
+
 }
